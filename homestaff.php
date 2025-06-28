85b1c0c60167f9850971aed821bf211bd366628c
SHREYA AGRAWAL <113137959+shreya4112@users.noreply.github.com>
2025-06-28 23:51:21 +0530

    Update homestaff.php

--- homestaff.php
+++ homestaff.php
@@ -1,8 +1,8 @@
 <html>
 
 <head>
-    <title>
-        Onine examination System
+    <title> 
+        ONLINE EXAMINATIONS SYSTEM
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
